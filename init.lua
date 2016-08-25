3f32bcda0cbf3210ccecba031339021532ed78eb
Karl Becker <karlbecker87@gmail.com>
2016-08-25 14:16:19 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,301 @@
+-----------------------------------------------
+-- Hammerspoon Karl 12.8.16
+-----------------------------------------------
+hs.window.animationDuration = 0
+-- Damit Fokus funktioniert: (?!?)
+require 'action'
+require 'profile'
+local application = require "hs.application"
+local fnutils = require "hs.fnutils"
+local grid = require "hs.grid"
+local hotkey = require "hs.hotkey"
+local mjomatic = require "hs.mjomatic"
+local window = require "hs.window"
+local tiling = require "hs.tiling"
+local main_monitor = "Color LCD"
+local second_monitor = "VX24A"
+----------------------------------------------
+-- Configure Grid
+----------------------------------------------
+hs.grid.setMargins({0, 0})
+hs.grid.setGrid('8x4','2560x1440')
+hs.grid.setGrid('6x4')
+hs.grid.ui.cellStrokeWidth = 0
+hs.grid.ui.cellColor = {0,0,0,0.25}
+hs.grid.ui.selectedColor = {0.925490,0.921569,0.921569,0.5}
+hs.grid.ui.highlightColor = {0.8,0.8,0,0.5}
+hs.grid.ui.textSize = 96
+hs.grid.ui.fontName = 'TheSans'
+hs.grid.ui.highlightStrokeWidth = 0
+hs.grid.HINTS = {
+  {'1', '2', '3', '4', '5', '6', '7', '8'},
+  {'1', '2', '3', '4', '5', '6', '7', '8'},
+  {'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I'},
+  {'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K'},
+  {'Y', 'X', 'C', 'V', 'B', 'N', 'M', ','}
+}
+----------------------------------------------
+-- Modifier shortcuts
+----------------------------------------------
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+local move = {"alt", "ctrl"}
+local size = {"ctrl", "cmd"}
+local push = {"alt", "cmd"}
+local mash = {"cmd", "alt", "ctrl"}
+-- hide window shadows
+hs.window.setShadows(false)
+----------------------------------------------
+
+-- Show Grid
+hs.hotkey.bind(mash, 'g',     function() hs.grid.toggleShow() end)
+-- Snap single window to grid
+hs.hotkey.bind(mash, ',',     function() grid.snap(window.focusedWindow()) end)
+-- Snap all windows to grid
+hs.hotkey.bind(mash, '.',     function() fnutils.map(window.visibleWindows(), grid.snap) end)
+
+-- --------------------------------------------------------
+
+-- --------------------------------------------------------
+-- Helper functions - these do all the heavy lifting below.
+-- --------------------------------------------------------
+
+
+
+-- Resize a window by moving the bottom
+function yank(xpixels,ypixels)
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+
+	f.w = f.w + xpixels
+	f.h = f.h + ypixels
+	win:setFrame(f)
+end
+
+----------------------------------------------
+-- Movement hotkeys
+
+hs.hotkey.bind(move, 'down', function() grid.pushWindowDown() end) 	--down
+hs.hotkey.bind(move, "up", function() grid.pushWindowUp() end)	--up
+hs.hotkey.bind(move, "right", function() grid.pushWindowRight() end)	--right
+hs.hotkey.bind(move, "left", function() grid.pushWindowLeft() end)	--left
+
+-- hs.hotkey.bind(move, 'down', function() nudge(0,100) end)   --down
+-- hs.hotkey.bind(move, "up", function() nudge(0,-100) end)  --up
+-- hs.hotkey.bind(move, "right", function() nudge(100,0) end)  --right
+-- hs.hotkey.bind(move, "left", function() nudge(-100,0) end)  --left
+
+-- Resize hotkeys
+
+-- hs.hotkey.bind(size, "up", function() yank(0,-100) end) -- yank bottom up
+-- hs.hotkey.bind(size, "down", function() yank(0,100) end) -- yank bottom down
+-- hs.hotkey.bind(size, "right", function() yank(100,0) end) -- yank right side right
+-- hs.hotkey.bind(size, "left", function() yank(-100,0) end) -- yank right side left
+
+---------------------------------------
+-- Left
+
+hs.hotkey.bind({"cmd", "alt"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-- 1/3 Left
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 3
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-- Top
+
+hs.hotkey.bind({"cmd", "alt"}, "Up", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = 0
+    f.y = 0
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+-- Bottom
+
+hs.hotkey.bind({"cmd", "alt"}, "Down", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = 0
+    f.y = max.y + (max.h / 2)
+    f.w = max.w
+    f.h = max.h / 2
+    win:setFrame(f)
+end)
+
+-- Right
+
+hs.hotkey.bind({"cmd", "alt"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+-----------------------------------------------
+-- Fullscreen
+-----------------------------------------------
+
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "Up", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- Special Positions
+-----------------------------------------------
+
+hs.hotkey.bind(mash, "4", function()
+     local win = hs.window.focusedWindow()
+     local f = win:frame()
+     local screen = win:screen()
+     local max = screen:frame()
+ 
+     f.x = 145
+     f.y = 0
+     f.w = max.w - 280
+     f.h = max.h
+     win:setFrame(f)
+end)
+-- Center
+hs.hotkey.bind(mash, "Space", function()
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+ 
+      f.x = max.w / 8
+      f.y = max.h / 12
+      f.w = max.w * 0.75
+      f.h = max.h * 0.85
+      win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- Window Layouts
+-----------------------------------------------
+
+local function center(window)
+	   frame = window:screen():frame()
+	   frame.x = (frame.w / 2) - (frame.w / 4)
+	   frame.y = (frame.h / 2) - (frame.h / 4)
+	   frame.w = screen.w / 2
+	   frame.h = screen.h / 2
+	   window:setFrame(frame)
+end
+
+-- Float & Center
+
+hotkey.bind(mash, "f", function() tiling.toggleFloat(center) end)
+
+-- Cycle Layouts
+
+hotkey.bind(mash, "c", function() tiling.cycleLayout() end)
+hotkey.bind(mash, "v", function() tiling.cycle(1) end)
+hotkey.bind(mash, "x", function() tiling.cycle(-1) end)	 
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, "i", function()
+    hs.hints.windowHints()
+end)
+
+--		saved.win 		The window last moved.
+--		saved.winframe The frame for the window before moving.
+
+saved = {}
+saved.win = {}
+saved.winframe = {}
+
+-- Description:      Move the current window to the right 1/3rd of the screen.
+
+function zweidrittel()
+   saved.win = hs.window.focusedWindow()
+   saved.winframe = saved.win:frame()
+	hs.grid.set(saved.win, {x = 0, y = 0, w = 4, h = 5}, hs.screen.mainScreen())
+end
+
+hs.hotkey.bind({"ctrl", "alt", "cmd"},"Left", zweidrittel)
+
+-- Description:      Move the current window to the left 2/3rds of the screen.
+
+function eindrittel()
+   saved.win = hs.window.focusedWindow()
+   saved.winframe = saved.win:frame()
+	hs.grid.set(hs.window.focusedWindow(), {x = 4, y = 0, w = 2, h = 5}, hs.screen.mainScreen())
+end
+
+hs.hotkey.bind({"ctrl", "alt", "cmd"},"Right", eindrittel)
+----------------------------------------------
+-- change focusWindowWest
+
+hs.hotkey.bind(hyper, 'Left', function() hs.window.focusedWindow():focusWindowWest() end)
+hs.hotkey.bind(hyper, 'Right', function() hs.window.focusedWindow():focusWindowEast() end)
+hs.hotkey.bind(hyper, 'Up', function() hs.window.focusedWindow():focusWindowNorth() end)
+hs.hotkey.bind(hyper, 'Down', function() hs.window.focusedWindow():focusWindowSouth() end)
+
+----------------------------------------------
+-- Launch or Focus Safari & Canary (Mail)
+
+hs.hotkey.bind(mash, "b", function() hs.application.launchOrFocus("Safari") end)
+hs.hotkey.bind(mash, "m", function() hs.application.launchOrFocus("Canary") end)
+
+-----------------------------------------------
+-- Reload config on write
+-----------------------------------------------
+
+hotkey.bind(mash, "R", function()
+  hs.reload()
+end)
+
+function reload_config(files)
+    hs.reload()
+end
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+
+hs.alert.show("Config loaded")
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Hammerspoon-Config
+
+My personal Hammerspoon Config
+
+- ctrl + cmd + arrows: resize window
+- ctrl + alt + arrows: move window
+- alt + cmd + arrows: split in halfs (top/bottom/left/right)
+
+### mash = ctrl + alt + cmd
+
+- mash + right arrow: right third
+- mash + left arrow: left, two thirds
+- mash + up arrow: full screen
+- mash + 4: middle of the screen
+- mash + space: center
+- mash + f,c,j,k: Window Layouts
+
+## Using [hyperkey](http://brettterpstra.com/2012/12/08/a-useful-caps-lock-key/)
+
+- hyper + i: Window hints
+- hyper + arrows: Focus
